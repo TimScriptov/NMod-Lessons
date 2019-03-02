8afb02651bde94ce8ed2304bb17644532c12275c
TimScriptov <timscriptov@mail.ru>
2019-03-02 23:24:34 +0500

    ...

--- app/src/main/java/com/mcal/mcdesign/utils/BitmapRepeater.java
+++ app/src/main/java/com/mcal/mcdesign/utils/BitmapRepeater.java
@@ -1,9 +1,30 @@
+/*
+ * Copyright (C) 2018-2019 Тимашков Иван
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mcal.mcdesign.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 
+//##################################################################
+/**
+ * @author Тимашков Иван
+ * @author https://github.com/TimScriptov
+ */
 public class BitmapRepeater
+//##################################################################
 {
 	private static Bitmap repeatW(int width, Bitmap src)
 	{
--- app/src/main/java/com/mcal/mcdesign/widget/MCDActionBarView.java
+++ app/src/main/java/com/mcal/mcdesign/widget/MCDActionBarView.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2018-2019 Тимашков Иван
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mcal.mcdesign.widget;
 
 import android.widget.RelativeLayout;
@@ -8,7 +23,13 @@ import android.graphics.drawable.BitmapDrawable;
 import com.mcal.mcdesign.utils.BitmapRepeater;
 import com.mcal.nmodlessons.R;
 
+//##################################################################
+/**
+ * @author Тимашков Иван
+ * @author https://github.com/TimScriptov
+ */
 public class MCDActionBarView extends RelativeLayout
+//##################################################################
 {
 	public MCDActionBarView(android.content.Context context)
 	{
